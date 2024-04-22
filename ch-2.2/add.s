aebd6d1c5fc775c144c327da910e7348d57e64c2
jumzzz <jummel19@gmail.com>
2024-04-22 15:27:47 +0800

    yes

--- ch-2.2/add.s
+++ ch-2.2/add.s
@@ -6,14 +6,14 @@ Disassembly of section __TEXT,__text:
 0000000100003f74 <_main>:
 100003f74: d10043ff    	sub	sp, sp, #16
 100003f78: 52800000    	mov	w0, #0
-100003f7c: b9000fff    	str	wzr, [sp, #12] ; Write contents of stackpointer sp to wzr 
+100003f7c: b9000fff    	str	wzr, [sp, #12]
 100003f80: 52800148    	mov	w8, #10
 100003f84: b9000be8    	str	w8, [sp, #8]
 100003f88: 52800288    	mov	w8, #20
 100003f8c: b90007e8    	str	w8, [sp, #4]
-100003f90: b9400be8    	ldr	w8, [sp, #8] ; load 4 bytes
-100003f94: b94007e9    	ldr	w9, [sp, #4] ; load 4 bytes
-100003f98: 0b090108    	add	w8, w8, w9   ; add w8 and w9 and save the result to w8 register
+100003f90: b9400be8    	ldr	w8, [sp, #8]
+100003f94: b94007e9    	ldr	w9, [sp, #4]
+100003f98: 0b090108    	add	w8, w8, w9
 100003f9c: b90003e8    	str	w8, [sp]
 100003fa0: 910043ff    	add	sp, sp, #16
 100003fa4: d65f03c0    	ret
